6ce43039acb1de2fe101c692b27c34fce6b1f1b2
Maxence Raballand <maxenceraballand00@gmail.com>
2022-12-03 20:34:58 -0500

    Added animations on notifications

--- hooks/notifications.tsx
+++ hooks/notifications.tsx
@@ -57,38 +57,62 @@ function NotificationComponent(props: NotificationProps) {
 
     const { type, title, content } = props
 
-    if (!isVisible || props.isClosed) return <></>
-
     return (
-        <motion.div className='w-screen max-w-full rounded-lg bg-gray-900 shadow-md p-4 text-md flex space-x-2'>
-            {type === "loading" && (
-                <div role="status" className='m-auto'>
-                    <svg aria-hidden="true" className="w-6 h-6 animate-spin fill-c-dark" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-                    </svg>
-                    <span className="sr-only">Loading...</span>
-                </div>
-            )}
-            <div className='flex-grow'>
-                {title && (
-                    <div className={`font-semibold flex items-center space-x-2 ${type === "error" ? "text-red-500" : type === "success" ? "text-green-500" : ""}`}>
-                        {type === "default" && (<AiFillInfoCircle/>)}
-                        {type === "error" && (<AiFillCloseCircle/>)}
-                        {type === "success" && (<AiFillCheckCircle/>)}
-                        <div className='first-letter:uppercase'>{title}</div>
+        <AnimatePresence>
+            {(isVisible && !props.isClosed) && (
+                <motion.div 
+                    className='w-screen max-w-full rounded-lg bg-gray-900 shadow-md p-4 text-md flex space-x-2'
+                    initial={{
+                        opacity: 0,
+                        y: 10,
+                        x: 0
+                    }}
+                    animate={{
+                        opacity: 1,
+                        y: 0,
+                        x:0
+                    }}
+                    exit={{
+                        opacity: 0,
+                        y: 0,
+                        x: 30
+                    }}
+                    key={props.id}
+                    transition={{
+                        type: 'tween',
+                        duration: .2
+                    }}
+                >
+                    {type === "loading" && (
+                        <div role="status" className='m-auto'>
+                            <svg aria-hidden="true" className="w-6 h-6 animate-spin fill-c-dark" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                            </svg>
+                            <span className="sr-only">Loading...</span>
+                        </div>
+                    )}
+                    <div className='flex-grow'>
+                        {title && (
+                            <div className={`font-semibold flex items-center space-x-2 ${type === "error" ? "text-red-500" : type === "success" ? "text-green-500" : ""}`}>
+                                {type === "default" && (<AiFillInfoCircle/>)}
+                                {type === "error" && (<AiFillCloseCircle/>)}
+                                {type === "success" && (<AiFillCheckCircle/>)}
+                                <div className='first-letter:uppercase'>{title}</div>
+                            </div>
+                        )}
+                        <div className='text-sm'>
+                            {content}
+                        </div>
                     </div>
-                )}
-                <div className='text-sm'>
-                    {content}
-                </div>
-            </div>
-            <div>
-                <button className='bg-gray-700 p-2 rounded-full text-gray-400 hover:bg-gray-600 transition' onClick={() => {setIsVisible(false)}}>
-                    <RxCross2/>
-                </button>
-            </div>
-        </motion.div>
+                    <div>
+                        <button className='bg-gray-700 p-2 rounded-full text-gray-400 hover:bg-gray-600 transition' onClick={() => {setIsVisible(false)}}>
+                            <RxCross2/>
+                        </button>
+                    </div>
+                </motion.div>
+            )}
+        </AnimatePresence>
     )
 }
 
@@ -149,14 +173,12 @@ export default function NotificationsProvider({ children }: NotificationsProvide
         }}>
             {children}
             <div className='fixed bottom-0 right-0 space-y-2 max-w-sm p-2 z-[51]'>
-                <AnimatePresence>
-                    {notifications.map(notif => (
-                        <NotificationComponent
-                            {...notif}
-                            key={notif.id}
-                        />
-                    ))}
-                </AnimatePresence>
+                {notifications.map(notif => (
+                    <NotificationComponent
+                        {...notif}
+                        key={notif.id}
+                    />
+                ))}
             </div>
         </NotificationsContext.Provider>
     )
